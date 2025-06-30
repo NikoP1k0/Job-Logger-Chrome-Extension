7e73044abab219ead53579df141c0b8ae1e2334b
Nikolas <nikolas.kolic@gmail.com>
2025-06-30 13:22:14 +0200

    Added some new elements to the popup (input text, input date, labels, buttons). Also added an event listener for the log job button.

--- project files/popup.js
+++ project files/popup.js
@@ -1,1 +1,9 @@
-console.log('This is a popup!');
+document.addEventListener('DOMContentLoaded', function() {
+  var logBtn = document.getElementById('loggingButton');
+  if (logBtn) {
+    logBtn.addEventListener('click', function() {
+      document.getElementById('helloSection').style.display = 'none';
+      document.getElementById('formSection').style.display = 'block';
+    });
+  }
+});
